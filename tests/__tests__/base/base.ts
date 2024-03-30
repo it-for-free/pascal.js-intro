2ce738a37af5cdb0c5124f43cbbbc4158cc79875
vedro-compota <ru-ibn-hottab@yandex.ru>
2024-03-30 18:05:02 +0300

    continue

--- tests/__tests__/base/base.ts
+++ tests/__tests__/base/base.ts
@@ -4,5 +4,5 @@ import { runFile, insp } from '../../helpers/testsHelper';
 let pjs = runFile(import.meta.url, 'base.code');
 
 test('result = 18', () => {
-    expect(pjs.engine.results[0]).toBe(18);
-  });
+  expect(pjs.engine.results[0]).toBe(18);
+});
