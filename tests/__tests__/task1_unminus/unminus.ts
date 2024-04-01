90a59ae1d9fe92d6b51dc3547da51617e4ab6c37
vedro-compota <ru-ibn-hottab@yandex.ru>
2024-04-01 22:54:00 +0300

    Пример тестов для унарного минуса

---
+++ tests/__tests__/task1_unminus/unminus.ts
@@ -0,0 +1,31 @@
+
+import { runFile, insp } from '../../helpers/testsHelper';
+
+/**
+ * Тестируем поддержку унарного минуса
+ * См. задачу:
+ * @see https://fkn.ktu10.com/?q=node/16471 
+ * 
+ */
+
+let pjs = runFile(import.meta.url, 'unminus.code');
+
+test('result = -4', () => {
+  expect(pjs.engine.results[0]).toBe(-4);
+});
+
+test('result = -2', () => {
+  expect(pjs.engine.results[0]).toBe(-2);
+});
+
+test('result = -2', () => {
+  expect(pjs.engine.results[0]).toBe(-2);
+});
+
+test('result = -3', () => {
+  expect(pjs.engine.results[0]).toBe(-3);
+});
+
+test('result = 20', () => {
+  expect(pjs.engine.results[0]).toBe(20);
+});
